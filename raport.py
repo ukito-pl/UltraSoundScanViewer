a95fcfa9039486a4a8bb6df827c65220c14e3eae
Marek Buła <bulabot@gmail.com>
2019-04-07 22:59:33 +0200

    report

--- raport.py
+++ raport.py
@@ -24,11 +24,26 @@ class ReportDialog(QtGui.QDialog, RaportWindow.Ui_Dialog):
         self.connect(self.pushButton_new_report, SIGNAL('clicked()'), self.newFile)
         self.connect(self.pushButton_add_change, SIGNAL('clicked()'), self.addElementToReport)
         self.connect(self.pushButton_delete, SIGNAL('clicked()'), self.deleteCurrentElementFromReport)
+        self.treeWidget.itemClicked.connect(self.treeItemSelected)
         self.comboBox_type.currentIndexChanged.connect(self.setElementType)
         self.setElementType(self.comboBox_type.currentIndex())
         self.reportDir = -1
+        self.pushButton_delete.setEnabled(False)
 
-    def setTreeView(self):
+    def treeItemSelected(self,item, column):
+        if item.parent():
+            element_data = self.getElementData(item.text(1))
+            element_type_id = item.parent().data(1,QtCore.Qt.UserRole).toInt()
+            if element_type_id:
+                element_type_id = element_type_id[0]
+                self.setElementType(element_type_id)
+            self.setTableItems(element_data)
+            self.pushButton_delete.setEnabled(True)
+            self.pushButton_add_change.setText(_translate("Dialog", "Zmień".__str__(), None))
+        else:
+            self.pushButton_delete.setEnabled(False)
+
+    def setTreeWidget(self):
         self.treeWidget.clear()
         self.treeWidget.header().resizeSection(0,200)
         parent_item = self.treeWidget
@@ -39,6 +54,7 @@ class ReportDialog(QtGui.QDialog, RaportWindow.Ui_Dialog):
             if len(line) > 3 and line[0] == "Rodzaj:":
                 item = QtGui.QTreeWidgetItem(self.treeWidget)
                 item.setText(0, _translate("Dialog", line[1].__str__(), None))
+                item.setData(1, QtCore.Qt.UserRole, _translate("Dialog", line[3].__str__(), None))
                 parent_item = item
                 line = report.readline()
                 line = report.readline()
@@ -49,13 +65,14 @@ class ReportDialog(QtGui.QDialog, RaportWindow.Ui_Dialog):
                 line = report.readline()
             else:
                 line = report.readline()
+        report.close()
 
     def getFile(self):
         dir = QtGui.QFileDialog.getOpenFileName(filter="CSV files (*.csv)")
         if dir:
             self.reportDir = dir
             self.label_file_dir.setText(dir)
-            self.setTreeView()
+            self.setTreeWidget()
 
 
     def newFile(self):
@@ -70,37 +87,60 @@ class ReportDialog(QtGui.QDialog, RaportWindow.Ui_Dialog):
                 report.writelines(header1)
                 report.writelines(header2)
             report.close()
-            self.setTreeView()
+            self.setTreeWidget()
 
     def addElementToReport(self):
         lineData = ""
         for i in range(0,self.tableWidget.rowCount()):
             lineData = lineData + self.tableWidget.item(i,0).text() + ","
         lineData = lineData + "\n"
-        id = self.comboBox_type.currentIndex()
+        type_id = self.comboBox_type.currentIndex()
+        element_id = self.tableWidget.item(0,0).text()
         report = open(self.reportDir,"r")
         found = False
+        new = False
         j = -1
         i = 0
         report_content = report.readlines()
         for line in report_content:
             line = line.replace("\n","").split(',')
-            if len(line) >= 3 and line[3]==id.__str__():
+            if len(line) >= 3 and line[2] == "RodzajID:" and line[3]==type_id.__str__():
                 found = True
             if found and line[0] == '':
+                j = i
+                found = False
+                new = True
+            elif found and  line[0] == element_id.__str__():
                 j = i
                 found = False
             i = i+1
         report.close()
         if j != -1:
-            report_content.insert(j,lineData)
+            if new:
+                report_content.insert(j,lineData)
+            else:
+                report_content[j] = lineData
         report = open(self.reportDir, "w")
         report.writelines(report_content)
         report.close()
+        self.setTreeWidget()
+
+    def getElementData(self,id):
+        report = open(self.reportDir,'r')
+        for line in report:
+            line = line.replace("\n", "").split(',')
+            if len(line) > 1 and line[0] == id.__str__():
+                return line
+        report.close()
 
     def deleteCurrentElementFromReport(self):
-        id = self.tableWidget.item(0,0).text()
+        current_item = self.treeWidget.currentItem()
+        parent = current_item.parent()
+        id = current_item.text(1)
         self.deleteElementFromReport(id)
+        parent.takeChild(parent.indexOfChild(current_item))
+        self.pushButton_delete.setEnabled(False)
+        self.tableWidget.clearContents()
 
     def deleteElementFromReport(self,elem_id):
         report = open(self.reportDir, "r")
@@ -115,14 +155,14 @@ class ReportDialog(QtGui.QDialog, RaportWindow.Ui_Dialog):
         report.close()
         if j != -1:
             report_content.__delitem__(j)
-        print
         report = open(self.reportDir, "w")
         report.writelines(report_content)
         report.close()
 
     def setElementType(self,id):
         list = self.getTableItemsNamesList(id)
-        self.setTableItems(list)
+        self.setTableHeaderItems(list)
+        self.comboBox_type.setCurrentIndex(id)
 
     def getTableItemsNamesList(self,id):
         file = open("tableNamesList.txt",'r')
@@ -135,7 +175,18 @@ class ReportDialog(QtGui.QDialog, RaportWindow.Ui_Dialog):
         file.close
         return []
 
-    def setTableItems(self,item_names_list):
+    def setTableItems(self,item_list):
+        i = 0
+        for item_val in item_list:
+            try:
+                item = QtGui.QTableWidgetItem()
+                self.tableWidget.setItem(i,0,item)
+                self.tableWidget.item(i, 0).setText(item_val)
+            except:
+                pass
+            i = i + 1
+
+    def setTableHeaderItems(self, item_names_list):
         i = 0
         self.tableWidget.setRowCount(len(item_names_list))
         for name in item_names_list:
